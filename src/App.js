c0e5d2450e3c45f7b6d279b1c3577eace94124d8
MD.RUHEL KHAN <ruhel241@gmail.com>
2021-07-13 13:39:21 +0600

    added countdown timer

--- src/App.js
+++ src/App.js
@@ -2,47 +2,81 @@
 import './App.css';
 
 function App() {
+
+  // The data/time we want to countdown to
+  const countDownDate = new Date("Nov 10, 2021 10:00:00").getTime();
+
+  // Run myfunc every second
+  const myfunc = setInterval( () => {
+      
+      const now      = new Date().getTime();
+      const timeleft = countDownDate - now;
+          
+      // Calculating the days, hours, minutes and seconds left
+      const days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+      const hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+          
+      // Result is output to the specific element
+      document.getElementById("_timer_days").innerHTML = days
+      document.getElementById("_timer_hours").innerHTML = hours 
+      document.getElementById("_timer_minutes").innerHTML = minutes
+      document.getElementById("_timer_seconds").innerHTML = seconds 
+          
+      // Display the message when countdown is over
+      if (timeleft < 0) {
+          clearInterval(myfunc);
+          document.getElementById("_timer_days").innerHTML = ""
+          document.getElementById("_timer_hours").innerHTML = "" 
+          document.getElementById("_timer_minutes").innerHTML = ""
+          document.getElementById("_timer_seconds").innerHTML = ""
+          document.getElementById("end").innerHTML = "TIME UP!!";
+      }
+  }, 1000);
+
+
   return (
     <div className="App">
         <h1>Waiting Apps</h1>
+       
+        {/* <h2 id="end"></h2> */}
+
         <div className="_sale_booster_countdown_wrap _sale-booster-countdown-bottom">
           <p className="_sale-booster-hits"> Meet Your Husband left </p>
             
-            {/* <div className="timer_days_count">
-              <div className="_sale-discount-countdown-timer"> 
-                <span className="_sale-discount-time _timer_days">121</span>  
-                <span className="_sale-discount-units">days</span>
-              </div>
-            </div> */}
-
             <div className="_sale-booster-countdown">
               
               <div className="waiting_days_count">
                 <div className="_sale-discount-countdown-time"> 
-                  <span className="_sale-discount-time _timer_days ddd">120</span>  
+                  <span className="_sale-discount-time _timer_days ddd" id="_timer_days"></span>  
                   <span className="_sale-discount-units sss">days</span>
                 </div>
               </div>
 
               <div className="waiting-countdown">
                 <div className="_sale-discount-countdown-timer">  
-                  <span className="_sale-discount-time _timer_hours">01</span>  
+                  <span className="_sale-discount-time _timer_hours" id="_timer_hours"></span>  
                   <span className="_sale-discount-units">hours</span>
                 </div>
 
                 <div className="_sale-discount-countdown-timer">
-                <span className="_sale-discount-time _timer_minutes">36</span>  
+                <span className="_sale-discount-time _timer_minutes" id="_timer_minutes"></span>  
                   <span className="_sale-discount-units">minutes</span>
                 </div>
 
                 <div className="_sale-discount-countdown-timer">   
-                  <span className="_sale-discount-time _timer_seconds">00</span>  
+                  <span className="_sale-discount-time _timer_seconds" id="_timer_seconds"></span>  
                   <span className="_sale-discount-units">seconds</span>
                 </div>
               </div>
 
             </div>
-          <p className="_sale-booster-hits"> when the day and timer hits zero, your heatbeat will go so fast..... </p>
+          <p className="_sale-booster-hits"> When the day and timer hits zero, your heatbeat will go so fast..... </p>
+
+          <img src="/images/2.jpg" alt="asdfas"/>
+          
+
         </div>
     </div>
   );
