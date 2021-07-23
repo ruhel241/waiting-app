f996d6ea4320abbd4afb57253adef1cc911b667e
MD.RUHEL KHAN <ruhel241@gmail.com>
2021-07-23 12:31:55 +0600

    hours and minute total count

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ function App() {
           
       // Calculating the days, hours, minutes and seconds left
       const days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-      const hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+      // const hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const hours   = Math.floor((timeleft / (1000 * 60 * 60)));
+      // const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+      const minutes = Math.floor((timeleft  / (1000 * 60)));
       const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
           
       // Result is output to the specific element
