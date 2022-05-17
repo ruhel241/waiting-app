216c87e512e09a0b18a7b08fd1f32abf476ee9ad
MD.RUHEL KHAN <ruhel241@gmail.com>
2022-05-17 21:40:14 +0600

    added dynamic date time

--- src/App.js
+++ src/App.js
@@ -1,95 +1,147 @@
 // import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import $ from 'jquery';
+import DateTimePicker from 'react-datetime-picker';
 
-function App() {
+class App extends Component  {
 
-  // The data/time we want to countdown to
-  const countDownDate = new Date("June 17, 2022 10:30:00").getTime();
+  constructor(props) {
+    super(props);
+    this.state = {
+      endDateTime: ''
+    }
+  }
 
-  // Run myfunc every second
-  const myfunc = setInterval( () => {
-      
-      const now      = new Date().getTime();
-      const timeleft = countDownDate - now;
-          
-      // Calculating the days, hours, minutes and seconds left
-      const days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-      // const hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const hours   = Math.floor((timeleft / (1000 * 60 * 60)));
-      // const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-      const minutes = Math.floor((timeleft  / (1000 * 60)));
-      const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-          
-      // Result is output to the specific element
-      document.getElementById("_timer_days").innerHTML = days
-      document.getElementById("_timer_hours").innerHTML = hours 
-      document.getElementById("_timer_minutes").innerHTML = minutes
-      document.getElementById("_timer_seconds").innerHTML = seconds 
-          
-      // Display the message when countdown is over
-      if (timeleft < 0) {
-          clearInterval(myfunc);
-          document.getElementById("_timer_days").innerHTML = ""
-          document.getElementById("_timer_hours").innerHTML = "" 
-          document.getElementById("_timer_minutes").innerHTML = ""
-          document.getElementById("_timer_seconds").innerHTML = ""
-          document.getElementById("end").innerHTML = "TIME UP!!";
-      }
-  }, 1000);
+  componentDidMount(){
+    this.getCategory();
+  }
+ 
+  getCategory = () => {
+    var category = '"Partner"'; 
+    $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
+      if (data.country_name === 'Bangladesh') {
+         category = '"Wife"'
+      } else if (data.country_name === 'United States') {
+       category = '"Husband"'
+      } 
+      document.getElementById("category").innerHTML = category;
+    });
+  };
 
-  $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
-    let category = '"Partner"';
-    if(data.country_name === 'Bangladesh') {
-      category = '"Wife"';
-    } else if (data.country_name === 'United States') {
-      category = '"Husband"';
-    } 
-    document.getElementById("category").innerHTML = category;
-  });
+  render() {
+    const {
+      endDateTime
+    } = this.state;
 
-  return (
-    <div className="App">
-        <h1 className="apps-name">Waiting Apps</h1>
-       
-        <div className="_sale_booster_countdown_wrap _sale-booster-countdown-bottom">
-          <p className="_sale-booster-hits">
-            You are waiting to meet your <span id="category"></span> and the time is left
-          </p>
+    const changeHandler = (e) => {
+      console.log(e);
+      this.setState({
+        endDateTime: e
+      })
+    }
+
+    const submitHandler = () => {
+      let EndDate = new Date(endDateTime).getTime();
+      localStorage.setItem('WaitingEndDate', EndDate);
+      window.location.reload(true);
+    }
+
+    const resetTimer = () => {
+        localStorage.removeItem('WaitingEndDate');
+        window.location.reload(true);
+    }
+
+    var getCountDownDate = localStorage.getItem('WaitingEndDate');
+    
+    if (!getCountDownDate) {
+      var present_date = new Date();
+      getCountDownDate  = present_date.setDate(present_date.getDate() - 1);
+    }
+
+    // Run myfunc every second
+    const myfunc = setInterval( () => {
+        const now      = new Date().getTime();
+        const timeleft = getCountDownDate - now;
+        // Calculating the days, hours, minutes and seconds left
+        const days    = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        // const hours   = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const hours   = Math.floor((timeleft / (1000 * 60 * 60)));
+        // const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+        const minutes = Math.floor((timeleft  / (1000 * 60)));
+        const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
             
-            <div className="_sale-booster-countdown">
-              <div className="waiting_days_count">
-                <div className="_sale-discount-countdown-time"> 
-                  <span className="_sale-discount-time _timer_days ddd" id="_timer_days"></span>  
-                  <span className="_sale-discount-units sss">days</span>
-                </div>
-              </div>
-              <div className="waiting-countdown">
-                <div className="_sale-discount-countdown-timer">  
-                  <span className="_sale-discount-time _timer_hours" id="_timer_hours"></span>  
-                  <span className="_sale-discount-units">hours</span>
-                </div>
-                <div className="_sale-discount-countdown-timer">
-                <span className="_sale-discount-time _timer_minutes" id="_timer_minutes"></span>  
-                  <span className="_sale-discount-units">minutes</span>
-                </div>
+        // Result is output to the specific element
+        document.getElementById("_timer_days").innerHTML = days
+        document.getElementById("_timer_hours").innerHTML = hours 
+        document.getElementById("_timer_minutes").innerHTML = minutes
+        document.getElementById("_timer_seconds").innerHTML = seconds 
+            
+        // Display the message when countdown is over
+          if (timeleft < 0) {
+            clearInterval(myfunc);
+            document.getElementById("_timer_days").innerHTML = 0
+            document.getElementById("_timer_hours").innerHTML = 0 
+            document.getElementById("_timer_minutes").innerHTML = 0
+            document.getElementById("_timer_seconds").innerHTML = 0
+          }
+    }, 1000);
 
-                <div className="_sale-discount-countdown-timer">   
-                  <span className="_sale-discount-time _timer_seconds" id="_timer_seconds"></span>  
-                  <span className="_sale-discount-units">seconds</span>
-                </div>
-              </div>
+    return (
+      <div className="App">
+          <h1 className="apps-name">Waiting App</h1>
+          <div className="end-date-time-box">
+            <div className="date-time">
+              <label> End Time </label>
+              <DateTimePicker onChange={changeHandler} value={endDateTime} />
             </div>
-          <p className="_sale-booster-hits">
-             When the day and timer hits zero, your heartbeat will go so fast.....
-          </p>
+            
+            <div className="date-time-btn">
+              <button className="submit-button" onClick={submitHandler}>Submit</button>
+              <button className="reset-button" onClick={resetTimer}>Reset</button>
+            </div>
+          </div>
 
-          <div className="image"> 
-            <img src="/images/2.jpg" alt="asdfas"/>
-          </div>  
-        </div>
-    </div>
-  );
+          <div className="_sale_booster_countdown_wrap _sale-booster-countdown-bottom">
+            <p className="_sale-booster-hits">
+              You are waiting to meet your <span id="category"></span> and the time is left
+            </p>
+              
+              <div className="_sale-booster-countdown">
+                <div className="waiting_days_count">
+                  <div className="_sale-discount-countdown-time"> 
+                    <span className="_sale-discount-time _timer_days ddd" id="_timer_days"></span>  
+                    <span className="_sale-discount-units sss">days</span>
+                  </div>
+                </div>
+                <div className="waiting-countdown">
+                  <div className="_sale-discount-countdown-timer">  
+                    <span className="_sale-discount-time _timer_hours" id="_timer_hours"></span>  
+                    <span className="_sale-discount-units">hours</span>
+                  </div>
+                  <div className="_sale-discount-countdown-timer">
+                  <span className="_sale-discount-time _timer_minutes" id="_timer_minutes"></span>  
+                    <span className="_sale-discount-units">minutes</span>
+                  </div>
+
+                  <div className="_sale-discount-countdown-timer">   
+                    <span className="_sale-discount-time _timer_seconds" id="_timer_seconds"></span>  
+                    <span className="_sale-discount-units">seconds</span>
+                  </div>
+                </div>
+              </div>
+            <p className="_sale-booster-hits">
+                When the day and timer hits zero, your heartbeat will go so fast.....
+            </p>
+
+            <div className="image"> 
+              <img src="/images/2.jpg" alt="asdfas"/>
+            </div>  
+          </div>
+      </div>
+    )
+  }
+    
+  
 }
-
 export default App;
